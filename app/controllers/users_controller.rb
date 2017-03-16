fc909d0cdcb993cc696ccb4c1769fd439303e13c
dkulpa <d.kulpa@selleo.com>
2017-03-16 13:22:03 +0100

    stage-7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"user-list\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
+    end
   end
 
   def destroy
